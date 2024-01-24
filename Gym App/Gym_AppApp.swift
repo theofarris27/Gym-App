142c9f1fc373adf95a761a5daaae32e1eac2e809
theofarris <theoisawesome1@gmail.com>
2024-01-24 13:28:32 -0800

    Initial Commit

---
+++ Gym App/Gym_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_AppApp.swift
+//  Gym App
+//
+//  Created by Theo Farris on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Gym_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
