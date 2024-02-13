b0b9292e075fd85177be4d5fc75c9f8a58648ae7
theofarris <theoisawesome1@gmail.com>
2024-02-13 13:52:08 -0800

    moved conext menu into its own file just in case

---
+++ Gym App/contextMenu.swift
@@ -0,0 +1,25 @@
+//
+//  contextMenu.swift
+//  Gym App
+//
+//  Created by Theo Farris on 2/13/24.
+//VStack {
+//
+//            Button {
+//
+//            } label: {
+//                Text("please work")
+//                    .multilineTextAlignment(.trailing)
+//                    .lineLimit(nil)
+//                    .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
+//                    .contextMenu(shouldShowMenu ? menuItems : nil)
+//                    .position(CGPoint(x: 300.0, y: 10.0))
+//            }
+//        }
+//        .contextMenu(ContextMenu(menuItems: {
+//            Text("Sign In")
+//            Text("Sign out")
+//            Text("Tracker")
+//        }))
+//      .padding()
+//  }
