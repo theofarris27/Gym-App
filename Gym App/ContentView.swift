23ae41d0cbb56787e6d41bdd1d08b6b6b7741756
theofarris <theoisawesome1@gmail.com>
2024-02-15 13:27:57 -0800

    sign in page looks but doesnt work

--- Gym App/ContentView.swift
+++ Gym App/ContentView.swift
@@ -10,29 +10,106 @@ import SwiftUI
 struct ContentView: View {
     @State private var shouldShowMenu = true
     @State private var selectedTab: Tab = .camera
+    @State var name: String = ""
+    @State var password: String = ""
+    @State var showPassword: Bool = false
     
+    var isSignInButtonDisabled: Bool {
+          [name, password].contains(where: \.isEmpty)
+      }
+      
+
     init() {
         UITabBar.appearance().isHidden = true
     }
     
     var body: some View {
-        
         ZStack{
             VStack{
                 TabView(selection: $selectedTab) {
                     ForEach(Tab.allCases, id: \.rawValue) { tab in
-                        HStack{
-                            Image(systemName: tab.rawValue)
-                            Text("\(tab.rawValue.capitalized)")
-                                .bold()
-                                .animation(nil, value: selectedTab)
+                        VStack{
+                            if(tab.rawValue == "leaf") {
+                                Spacer()
+                                           
+                                           TextField("Name",
+                                                     text: $name ,
+                                                     prompt: Text("Login").foregroundColor(.blue)
+                                           )
+                                           .padding(10)
+                                           .overlay {
+                                               RoundedRectangle(cornerRadius: 10)
+                                                   .stroke(.blue, lineWidth: 2)
+                                           }
+                                           .padding(.horizontal)
+
+                                           HStack {
+                                               Group {
+                                                   if showPassword {
+                                                       TextField("Password", // how to create a secure text field
+                                                                   text: $password,
+                                                                   prompt: Text("Password").foregroundColor(.red)) // How to change the color of the TextField Placeholder
+                                                   } else {
+                                                       SecureField("Password", // how to create a secure text field
+                                                                   text: $password,
+                                                                   prompt: Text("Password").foregroundColor(.red)) // How to change the color of the TextField Placeholder
+                                                   }
+                                               }
+                                               .padding(10)
+                                               .overlay {
+                                                   RoundedRectangle(cornerRadius: 10)
+                                                       .stroke(.red, lineWidth: 2) // How to add rounded corner to a TextField and change it colour
+                                               }
+
+                                               Button {
+                                                   showPassword.toggle()
+                                               } label: {
+                                                   Image(systemName: showPassword ? "eye.slash" : "eye")
+                                                       .foregroundColor(.red) // how to change image based in a State variable
+                                               }
+
+                                           }.padding(.horizontal)
+Spacer()
+                                           Button {
+                                               print("do login action")
+                                               
+                                           } label: {
+                                               Text("Sign In")
+                                                   .font(.title2)
+                                                   .bold()
+                                                   .foregroundColor(.white)
+                                           }
+                                           .frame(height: 50)
+                                           .frame(maxWidth: .infinity) // how to make a button fill all the space available horizontaly
+                                           .background(
+                                               isSignInButtonDisabled ? // how to add a gradient to a button in SwiftUI if the button is disabled
+                                               LinearGradient(colors: [.gray], startPoint: .topLeading, endPoint: .bottomTrailing) :
+                                                   LinearGradient(colors: [.blue, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+                                           )
+                                           .cornerRadius(20)
+                                           .disabled(isSignInButtonDisabled) // how to disable while some condition is applied
+                                           .padding()
+                                Spacer()
+                            }
+                            else if(tab.rawValue == "camera"){
+                                    Image(systemName: tab.rawValue)
+                                    Text("\(tab.rawValue.capitalized)")
+                                        .bold()
+                                        .animation(nil, value: selectedTab)
+                                }
+                            else {
+                                    Text("login")
+                                .position(CGPoint(x: 10.0, y: 10.0))
+                                }
                         }.tag(tab)
                     }
                 }
             }
-            VStack {
-                Spacer()
-                navigationBarTest(selectedTab: $selectedTab)
+                VStack {
+                    Spacer()
+                    navigationBarTest(selectedTab: $selectedTab)
+                }
+                
             }
         }
     }
@@ -56,4 +133,4 @@ struct ContentView: View {
     #Preview {
         ContentView()
     }
-}
+
--- Gym App/contextMenu.swift
+++ Gym App/contextMenu.swift
@@ -3,23 +3,18 @@
 //  Gym App
 //
 //  Created by Theo Farris on 2/13/24.
-//VStack {
-//
-//            Button {
-//
-//            } label: {
-//                Text("please work")
-//                    .multilineTextAlignment(.trailing)
-//                    .lineLimit(nil)
-//                    .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
-//                    .contextMenu(shouldShowMenu ? menuItems : nil)
-//                    .position(CGPoint(x: 300.0, y: 10.0))
-//            }
-//        }
-//        .contextMenu(ContextMenu(menuItems: {
-//            Text("Sign In")
-//            Text("Sign out")
-//            Text("Tracker")
-//        }))
-//      .padding()
-//  }
+//Button {
+//    
+//} label: {
+//    Text("please work")
+//        .multilineTextAlignment(.trailing)
+//        .lineLimit(nil)
+//        .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
+//        .contextMenu(shouldShowMenu ? menuItems : nil)
+//        .position(CGPoint(x: 10.0, y: -500.0))
+//}
+//.contextMenu(ContextMenu(menuItems: {
+//    Text("Sign In")
+//    Text("Sign out")
+//    Text("Tracker")
+//}))
