b0ac9098f085b9335567ef801c5f529e50e6e040
Sam <samlim1104@gmail.com>
2024-03-05 14:14:28 -0800

    camera stuff view

---
+++ Gym App/cameraView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  SwiftUICamera
+//
+
+import SwiftUI
+
+struct cameraView: View {
+    
+    
+    
+    @State private var showSheet: Bool = false
+    @State private var showImagePicker: Bool = false
+    @State private var sourceType: UIImagePickerController.SourceType = .camera
+    
+    @State private var image: UIImage?
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            VStack {
+
+                Image(uiImage: image ?? UIImage(named: "Levi")!)
+                    .resizable()
+                    .frame(width: 300, height: 300)
+                
+                Button("Choose Picture") {
+                    self.showSheet = true
+                }.padding()
+                    .actionSheet(isPresented: $showSheet) {
+                        ActionSheet(title: Text("Select Photo"), message: Text("Choose"), buttons: [
+                            .default(Text("Photo Library")) {
+                                self.showImagePicker = true
+                                self.sourceType = .photoLibrary
+                            },
+                            .default(Text("Camera")) {
+                                self.showImagePicker = true
+                                self.sourceType = .camera
+                            },
+                            .cancel()
+                        ])
+                }
+                
+            }
+                
+                
+            .navigationBarTitle("Camera Demo")
+            
+        }.sheet(isPresented: $showImagePicker) {
+            ImagePicker(image: self.$image, isShown: self.$showImagePicker, sourceType: self.sourceType)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
